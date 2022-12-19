7d2869d1ad358156dd22f42503ebc8c87e3641f7
wangdaxian <2313901135@qq.com>
2022-12-19 18:08:24 +0800

    格式化代码

--- DecoratorPattern.py
+++ DecoratorPattern.py
@@ -5,14 +5,17 @@
 
 from abc import ABCMeta, abstractmethod
 
+
 class Composition(metaclass=ABCMeta):
     @abstractmethod
     def operation(self):
         pass
 
+
 class ConcreteComposition(Composition):
     def operation(self):
         print("ConcreteComposition operation")  # 此处的operation是抽象方法
+
 
 class Decorator(Composition):
     def __init__(self, composition): 
@@ -21,17 +24,20 @@ class Decorator(Composition):
     def operation(self):
         self.composition.operation()
 
+
 class Decorator1(Decorator):
     def operation(self):
         d = Decorator(self.composition)
         d.operation()
         print("Decorator1 operation")
 
+
 class Decorator2(Decorator):
     def operation(self):
         d = Decorator(self.composition)
         d.operation()
         print("Decorator2 operation") 
+
 
 if __name__ == '__main__':
     composition = ConcreteComposition()
